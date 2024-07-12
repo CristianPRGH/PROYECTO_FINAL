8e6ab74b2bfff838535a4d271ad956b9d8345324
CristianPRGH <cpr.1992@gmail.com>
2024-07-12 08:37:32 +0200

    update

--- CUENTOS_tailwindcss/frontend/bookPageManager.js
+++ CUENTOS_tailwindcss/frontend/bookPageManager.js
@@ -4,17 +4,21 @@ document.addEventListener("DOMContentLoaded", ()=>{
     // document.getElementById("confirm-pages").addEventListener('click', lalala);
 
     // <textarea id="" class="bg-transparent w-full h-full p-2 outline-none text-justify overflow-x-hidden resize-none placeholder:italic" maxlength="1650" placeholder="Escribe aquí...(max 300 palabras)"></textarea>
-    const pagescontainer = document.getElementById("pages-container");
-    for (let index = 0; index < 10; index++)
-    {
-        const newPage = `<textarea id="page-${index}" class="bg-transparent w-full h-full p-2 outline-none text-justify overflow-x-hidden resize-none placeholder:italic" maxlength="1650" placeholder="Escribe aquí...(max 300 palabras)"></textarea>`
+    // const pagescontainer = document.getElementById("pages-container");
+    // for (let index = 0; index < 10; index++)
+    // {
         
-        if (index != 0)
-        {
-            newPage.classList.add("hidden");
-        }
-        pagescontainer.innerHTML += newPage;
-    }
+    //     // if (index != 0)
+    //     // {
+    //     //     newPage.classList.add("hidden");
+    //     // }
+    // }
+    // const newPage = `<textarea id="page-${index}" class="bg-transparent w-full h-full p-2 outline-none text-justify overflow-x-hidden resize-none placeholder:italic" maxlength="1650" placeholder="Escribe aquí...(max 300 palabras)"></textarea>`
+    // pagescontainer.innerHTML = newPage;
+
+    const quill = new Quill('#editor', {
+        theme: 'snow'
+    });
 })
 
 function PageControl(event)
